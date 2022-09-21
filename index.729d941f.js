1141468fc676b80975927acd494bd21fd7c41055
Lorengreyn <62342343+Lorengreyn@users.noreply.github.com>
2022-09-21 19:24:12 +0000

    Deploying to gh-pages from @ Lorengreyn/cv@c49dbe08cc86ddda925e80b069b3877abdfc814a 🚀

---
+++ index.729d941f.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){myFunction()};var element=document.getElementById("sidebar"),header=document.getElementById("myHeader"),fixed=element.offsetTop,sticky=header.offsetTop;function myFunction(){window.pageYOffset>=50?(element.classList.add("fixed"),header.classList.add("sticky")):(element.classList.remove("fixed"),header.classList.remove("sticky"))}
+//# sourceMappingURL=index.729d941f.js.map
