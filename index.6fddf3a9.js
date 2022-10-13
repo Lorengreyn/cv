52ab3a91cd202170e3945bc07aae792031780410
Lorengreyn <62342343+Lorengreyn@users.noreply.github.com>
2022-10-13 08:15:12 +0000

    Deploying to gh-pages from @ Lorengreyn/cv@5275d9ee62e75876229293c6a408c752fcd9fcc8 🚀

---
+++ index.6fddf3a9.js
@@ -0,0 +1,2 @@
+var link=document.getElementById("viber"),width=document.body.clientWidth;width>=1199&&link.setAttribute("href","viber://chat?number=+380677404300");
+//# sourceMappingURL=index.6fddf3a9.js.map
