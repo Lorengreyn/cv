5275d9ee62e75876229293c6a408c752fcd9fcc8
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-10-13 11:14:24 +0300

    1

---
+++ src/js/viber.js
@@ -0,0 +1,7 @@
+const link = document.getElementById("viber");
+
+const width = document.body.clientWidth;
+
+  if (width >= 1199) {
+    link.setAttribute('href','viber://chat?number=+380677404300');  
+  }
